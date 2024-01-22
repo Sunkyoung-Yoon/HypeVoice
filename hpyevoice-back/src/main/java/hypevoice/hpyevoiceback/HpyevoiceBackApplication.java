82e4c11198586c55ef5420724eba6529d4bc71b5
jaesik <wotlr5209@gamil.com>
2024-01-22 17:03:19 +0900

    feat : 프로젝트 베이스

---
+++ hpyevoice-back/src/main/java/hypevoice/hpyevoiceback/HpyevoiceBackApplication.java
@@ -0,0 +1,13 @@
+package hypevoice.hpyevoiceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpyevoiceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpyevoiceBackApplication.class, args);
+	}
+
+}
---
+++ hpyevoice-back/src/test/java/hypevoice/hpyevoiceback/HpyevoiceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package hypevoice.hpyevoiceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpyevoiceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
