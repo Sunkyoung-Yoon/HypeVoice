1eb002ce8b7e10172be604d70c8c7ea7a1dc54bd
gabin <3to@kakao.com>
2024-01-24 17:34:06 +0900

    feat : 스튜디오 엔티티 생성
    Change-Id: I3d782ed32f6a8e30e2de52867140341bcf5b9f0a

---
+++ hypevoice-back/src/main/java/hypevoice/hypevoiceback/studio/domain/StudioRepository.java
@@ -0,0 +1,7 @@
+package hypevoice.hypevoiceback.studio.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudioRepository extends JpaRepository<Studio, Long> {
+
+}
