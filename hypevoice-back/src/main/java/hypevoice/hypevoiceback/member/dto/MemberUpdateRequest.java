b3934de3c38c29ea9b7697a04babb4eaef1d6783
sunkyoung <yskkjdh@gmail.com>
2024-02-01 13:53:48 +0900

    refactor : @NotBlank, @Size  message 수정
    Change-Id: Ie0f41177fc63c1edb7144134057401982e589a10

--- hypevoice-back/src/main/java/hypevoice/hypevoiceback/member/dto/MemberUpdateRequest.java
+++ hypevoice-back/src/main/java/hypevoice/hypevoiceback/member/dto/MemberUpdateRequest.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.Size;
 
 public record MemberUpdateRequest(
         @NotBlank(message = "닉네임은 필수입니다.")
-        @Size(min = 1, message = "닉네임은 최소 1자 이내로 작성해주세요.")
-        @Size(max = 50, message = "닉네임은 10자 이내로 작성해주세요.")
+        @Size(min = 1, message = "닉네임은 최소 1자 이상으로 작성해주세요.")
+        @Size(max = 10, message = "닉네임은 10자 이내로 작성해주세요.")
         String nickname,
 
         String profileUrl
