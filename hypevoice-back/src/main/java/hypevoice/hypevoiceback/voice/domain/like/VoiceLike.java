a17a542d76c0d97d6430b057aaedf89b97029365
sunkyoung <yskkjdh@gmail.com>
2024-02-01 09:57:05 +0900

    refactor : VoiceLike PK 테이블 저장되는 이름 수정
    Change-Id: I081171b00fd30ddddc61484e208192464fc50774

--- hypevoice-back/src/main/java/hypevoice/hypevoiceback/voice/domain/like/VoiceLike.java
+++ hypevoice-back/src/main/java/hypevoice/hypevoiceback/voice/domain/like/VoiceLike.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class VoiceLike {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "voice_like_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
