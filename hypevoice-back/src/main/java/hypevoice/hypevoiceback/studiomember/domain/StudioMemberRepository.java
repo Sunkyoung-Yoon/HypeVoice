2734afadd785197335d09c756196c24d7a4566ab
sunkyoung <yskkjdh@gmail.com>
2024-02-05 12:53:10 +0900

    feat : StudioMemberRepository 메소드 추가
    Change-Id: I651058788e5812093ffc096859fcd4f2582e99e8

--- hypevoice-back/src/main/java/hypevoice/hypevoiceback/studiomember/domain/StudioMemberRepository.java
+++ hypevoice-back/src/main/java/hypevoice/hypevoiceback/studiomember/domain/StudioMemberRepository.java
@@ -2,10 +2,29 @@ package hypevoice.hypevoiceback.studiomember.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import hypevoice.hypevoiceback.member.domain.Member;
+import hypevoice.hypevoiceback.studio.domain.Studio;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface StudioMemberRepository extends JpaRepository<StudioMember,Long> {
 
-   boolean existsByMemberIdAndStudioId(Long memberId, Long studioId);
+    boolean existsByMemberIdAndStudioId(Long memberId, Long studioId);
 
-   void deleteByMemberIdAndStudioId(Long memberId, Long studioId);
+    void deleteByMemberIdAndStudioId(Long memberId, Long studioId);
+
+    @Query("select m " +
+            "from Member m left  join StudioMember sm " +
+            "on m.id = sm.member.id " +
+            "where sm.studio.id = :studioId")
+    List<Member> findAllByStudioId(Long studioId);
+
+    @Query("select s " +
+            "from Studio s left  join StudioMember sm " +
+            "on s.id = sm.studio.id " +
+            "where sm.member.id = :memberId")
+    Studio findByMemberId(Long memberId);
 
 }
