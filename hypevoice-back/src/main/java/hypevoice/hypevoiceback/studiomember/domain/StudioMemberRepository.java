6df6feadbbe433c31ee49a902f7b31ef251f4cb0
sunkyoung <yskkjdh@gmail.com>
2024-02-05 13:25:11 +0900

    feat : StudioMemberRepository 메소드 추가

--- hypevoice-back/src/main/java/hypevoice/hypevoiceback/studiomember/domain/StudioMemberRepository.java
+++ hypevoice-back/src/main/java/hypevoice/hypevoiceback/studiomember/domain/StudioMemberRepository.java
@@ -1,6 +1,11 @@
 package hypevoice.hypevoiceback.studiomember.domain;
 
+import hypevoice.hypevoiceback.member.domain.Member;
+import hypevoice.hypevoiceback.studio.domain.Studio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StudioMemberRepository extends JpaRepository<StudioMember,Long> {
 
@@ -8,4 +13,15 @@ public interface StudioMemberRepository extends JpaRepository<StudioMember,Long>
 
    void deleteByMemberIdAndStudioId(Long memberId, Long studioId);
 
+   @Query("select m " +
+           "from Member m left  join StudioMember sm " +
+           "on m.id = sm.member.id " +
+           "where sm.studio.id = :studioId")
+   List<Member> findAllByStudioId(Long studioId);
+
+   @Query("select s " +
+           "from Studio s left  join StudioMember sm " +
+           "on s.id = sm.studio.id " +
+           "where sm.member.id = :memberId")
+   Studio findByMemberId(Long memberId);
 }
